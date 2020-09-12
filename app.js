42ca43bd7c5beb10eac818cc5e06d74ddc7237e7
sidhi-sid <sidhisidh469@gmail.com>
2020-09-13 01:26:41 +0530

    user schema

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express=require ('express')
 const dotenv=require('dotenv')
 const morgan=require('morgan')
 const exphbs=require('express-handlebars')
+const session=require('express-session')
 const connectDB=require('./config/db')
+const passport=require('passport')
+
 dotenv.config({path:'./config/config.env'})
+
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -16,6 +21,16 @@ if(process.env.NODE_ENV==="development"){
 
 app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    
+  }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use('/',require('./routes/index'));
