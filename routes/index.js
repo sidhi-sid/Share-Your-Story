20885ebe57fef602de3ba36a91f59cec0c22098c
sidhi-sid <sidhisidh469@gmail.com>
2020-09-12 23:58:03 +0530

    login view

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ require('../views/login.hbs')
 
 
 router.get('/',(req,res)=>{
-    res.render('login');
+    res.render('login',{
+        layout:'login'
+    });
 })
 
 router.get('/dashboard',(req,res)=>{
