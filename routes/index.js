20a9fc0651509328d6ab676bd20fc5a96943383c
sidhi-sid <sidhisidh469@gmail.com>
2020-09-16 02:53:35 +0530

    show stories

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const router=express.Router()
 const { ensureAuth, ensureGuest}=require('../middleware/auth')
+const Story=require('../models/Story')
 require('../views/login.hbs')
 
 
@@ -10,11 +11,18 @@ router.get('/',ensureGuest,(req,res)=>{
     });
 })
 
-router.get('/dashboard',ensureAuth,(req,res)=>{
-
-    res.render('dashboard',{
-        name: req.user.firstName
-    });
+router.get('/dashboard',ensureAuth,async (req,res)=>{
+    try{
+        const stories=await Story.find({user: req.user.id}).lean()
+        res.render('dashboard',{
+            name: req.user.firstName,
+            stories
+        });
+    }catch(err){
+        console.error(err)
+        res.render('error/500')
+    }
+    
 })
 
 module.exports=router
