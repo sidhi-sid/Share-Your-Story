128ad224fff1d422ee4777f022cee64233cc1af6
sidhi-sid <sidhisidh469@gmail.com>
2020-09-18 20:43:38 +0530

    public stories feature

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ if(process.env.NODE_ENV==="development"){
     app.use(morgan('dev'))
 }
 
-app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}));
+const { formatDate , stripTags, truncate }=require('./helpers/hbs')
+
+app.engine('.hbs', exphbs({helpers: {formatDate, stripTags, truncate }, defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.use(session({
