341abe6c873fe371671da10c3a1b1920b05be9f2
sidhi-sid <sidhisidh469@gmail.com>
2020-09-19 01:45:12 +0530

    update feature added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 const morgan=require('morgan')
 const exphbs=require('express-handlebars')
+const methodOverride=require('method-override')
 const session=require('express-session')
 const connectDB=require('./config/db')
 const passport=require('passport')
@@ -20,13 +21,22 @@ const app=express()
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 if(process.env.NODE_ENV==="development"){
     app.use(morgan('dev'))
 }
 
-const { formatDate , stripTags, truncate }=require('./helpers/hbs')
+const { formatDate , stripTags, truncate, editIcon, select}=require('./helpers/hbs')
 
-app.engine('.hbs', exphbs({helpers: {formatDate, stripTags, truncate }, defaultLayout:'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs({helpers: {formatDate, stripTags, truncate, editIcon, select }, defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 app.use(session({
@@ -38,6 +48,11 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.use(function (req,res,next){
+  res.locals.user=req.user || null
+  next()
+})
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use('/',require('./routes/index'));
