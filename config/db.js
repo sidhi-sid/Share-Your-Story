d39f7c0257be9b6266ddae0efaeeb1d87a284922
Sidhi-sid <sidhi@prosperme.in>
2020-11-06 00:33:56 +0530

    deploy

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB=async()=>{
-    try{
-        const conn=await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true,
-            useFindAndModify:false
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://Sid:ssingh@cluster0.cvkdn.mongodb.net/stories?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
+    console.log(`MongoDB connected:${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
-        })
-        console.log(`MongoDB connected:${conn.connection.host}`)
-    }catch(err){
-        console.error(err)
-        process.exit(1)
-    }
-}
-
-module.exports=connectDB
+module.exports = connectDB;
